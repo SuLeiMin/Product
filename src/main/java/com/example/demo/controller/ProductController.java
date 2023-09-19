be7355964ffa90c7592972d3c8a1131a47392504
suleimin <suleimin@gmail.com>
2023-09-19 21:01:18 +0900

    Second of Product

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,7 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bookStore.bookStore.entity.Book;
+import com.example.demo.entity.Product;
 import com.example.demo.service.productService;
 
 @Controller
@@ -30,4 +36,36 @@ public class ProductController {
        
         return "menu";
     }
+	
+	@GetMapping("/mod")
+    public String regsiterform() {
+       
+        return "mod";
+    }
+	
+	/*
+	 * @GetMapping("/update_product") public String updateproduct() {
+	 * 
+	 * return "mod"; }
+	 */
+	
+	@RequestMapping("/update_product/{id}")
+	public String updateproduct1(@PathVariable("id") int id, Model model) {
+		Product p = pService.getBookById(id);
+		model.addAttribute("product", p);
+		return "mod";
+	}
+	
+	@GetMapping("/kakunin")
+	public String checkproduct() {
+		return "kakunin";
+	}
+	
+	@PostMapping("/register_product")
+	public String registerproduct(@ModelAttribute Product b) {
+		pService.save(b);
+
+		return "redirect:/mod";
+
+	}
 }
--- src/main/java/com/example/demo/service/productService.java
+++ src/main/java/com/example/demo/service/productService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bookStore.bookStore.entity.Book;
 import com.example.demo.entity.Product;
 import com.example.demo.repository.productRepository;
 
@@ -14,8 +15,17 @@ public class productService {
 	@Autowired 
 	private productRepository productRepo;
 	
-	 public ArrayList<Product> getAllProduct()
-	  {
+	 public ArrayList<Product> getAllProduct(){
 	        return (ArrayList<Product>) productRepo.findAll();
-	  }
+	 }
+	 
+	 public void save(Product p) {
+			productRepo.save(p);
+	 }
+	 
+	 public Product getBookById(int id) {
+			return productRepo.findById(id).get();
+	}
+		
+		
 }
